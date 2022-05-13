4c276b761c466b91122070b9bb3e22d11c7e1fc8
Joy L. Comesario <98447215+Comesariojoy@users.noreply.github.com>
2022-05-13 22:30:46 +0800

    Add files via upload

---
+++ Exer15 (1).js
@@ -0,0 +1,11 @@
+function displayAssignedValues(){
+    var firstName = document.getElementById("firstName").value = "Joy L. Comesario";
+    var age = document.getElementById("age").value = "21";
+    var phoneNumber = document.getElementById("phoneNumber").value = "09201066117";
+    var eMail = document.getElementById("eMail").value = "20215608@s.ubaguio.edu";
+    var mailAddress = document.getElementById("mailAddress").value = "San Carlos Hts. , Baguio City";
+    var program = document.getElementById("program").value = "iNFORMATION tECHNOLOGY";
+    var idNum = document.getElementById("idNum").value = "20139610";
+    var yearLevel = document.getElementById("yearLevel").value = "1st Year";
+    var section = document.getElementById("section").value = "IAA2";
+}
